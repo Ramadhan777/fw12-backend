be974309d05d8358704a8bd8239dc4741554cf62
Cupu-Boy <fajrir357@gmail.com>
2023-01-16 10:21:02 +0700

    fix profile controller

--- src/routes/profile.router.js
+++ src/routes/profile.router.js
@@ -1,11 +1,11 @@
 const profileRouter = require('express').Router()
 const authMiddleware = require('../middleware/auth.middleware')
 const uploadMiddleware = require('../middleware/upload.middleware')
-const {readProfile} = require('../controllers/profile.controller')
+const {readProfile, uploadImage} = require('../controllers/profile.controller')
 const {updateUser} = require('../controllers/users.controller')
 
 profileRouter.get('/', authMiddleware, readProfile)
 profileRouter.patch('/', authMiddleware, uploadMiddleware, updateUser)
-profileRouter.post('/upload', authMiddleware, uploadMiddleware, updateUser)
+profileRouter.patch('/upload', authMiddleware, uploadMiddleware, uploadImage)
 
 module.exports = profileRouter
