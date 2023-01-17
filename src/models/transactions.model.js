7960a25cac4e1147df93462f53d52ec5e4ef4ef5
Cupu-Boy <fajrir357@gmail.com>
2023-01-17 18:49:57 +0700

    update tranaction mmodel

--- src/models/transactions.model.js
+++ src/models/transactions.model.js
@@ -25,7 +25,7 @@ exports.selectTransaction = (data, callback) => {
 }
 
 exports.insertTransaction = (data, callback) => {
-  const sql = 'INSERT INTO transactions ("userId", "bookingDate", "movieId", "cinemaId", "fullName", "email", "phoneNumber", "paymentMethodId", "bookingTime", "totalPrice", "seatNum") VALUES ($1, $2, $3, $4, $5, $6, $7, $8, $9, $10. $11) RETURNING *';
+  const sql = 'INSERT INTO transactions ("userId", "bookingDate", "movieId", "cinemaId", "fullName", "email", "phoneNumber", "paymentMethodId", "bookingTime", "totalPrice", "seatNum") VALUES ($1, $2, $3, $4, $5, $6, $7, $8, $9, $10, $11) RETURNING *';
 
   const values = [data.userId, data.bookingDate, data.movieId, data.cinemaId, data.fullName, data.email, data.phoneNumber, data.paymentMethodId, data.bookingTime, data.totalPrice, data.seatNum]
 
